a93e595c88e58adfee2e6ddda95c75281d64bb47
Konstantin <mayorkos78@gmail.com>
2025-04-16 21:42:26 +0300

    first commit

--- Page 1.py
+++ Page 1.py
@@ -1,10 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+
 
 @pytest.fixture(scope='session')
 def browser():
@@ -13,25 +11,38 @@ def browser():
     yield driver
     driver.quit()
 
+
 class Page:
-    def __init__(self, browser):
-        self.browser = browser
-        self.locator = 'body > main > footer'
+    def __init__(self, driver):
+        self.driver = driver
+
+    footer = 'body > main > footer'
+    digital_in_footer = 'body > main > footer > div.Footer_grid__lfZ34 > svg'
+
+    def find_element(self, locator, locator_type='css'):
+        if locator_type == 'css':
+            by_type = By.CSS_SELECTOR
+        elif locator_type == 'xpath':
+            by_type = By.XPATH
+        else:
+            raise ValueError(f"Unsupported locator type: {locator_type}")
+
+        return self.driver.find_element(by_type, locator)
 
     def visit(self, url):
-        self.browser.get(url)
+        self.driver.get(url)
 
-    def find_element(self):
-        return self.browser.find_element(By.CSS_SELECTOR, self.locator)
-
-    def exist(self):
+    def exist(self, locator, locator_type='css'):
         try:
-            self.find_element()
+            self.find_element(locator, locator_type)
         except NoSuchElementException:
             return False
         return True
 
+
 def test_footer(browser):
     page = Page(browser)
     page.visit('https://only.digital/')
-    assert page.exist()
+    assert page.exist(page.footer)
+    assert page.exist(page.digital_in_footer)
+
